d3b24220054f7dee9db543fcd16c0c9d12354e41
kylejennergoldmansachs-ops <kylejennergoldmansachs@gmail.com>
2025-08-19 19:15:31 +0200

    Update MessageStore.kt

--- app/src/main/java/com/twistedphone/messages/MessageStore.kt
+++ app/src/main/java/com/twistedphone/messages/MessageStore.kt
@@ -10,6 +10,6 @@ object MessageStore {
     }
     fun allMessages(ctx: Context) = ctx.getSharedPreferences("tw_msgs",0).getStringSet("msgs",mutableSetOf())!!.toList().sorted()
     fun recentHistory(ctx: Context, limit: Int = 10): String {
-        return allMessages(ctx).takeLast(limit).joinToString("\n") { it.split("|", 3).let { p -> "${p[0]}: ${p[2]}" } }
+        return allMessages(ctx).takeLast(limit).joinToString("\n") { it.split("|", limit = 3).let { p -> "${p[0]}: ${p[2]}" } }
     }
 }
